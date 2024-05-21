131116d40683902238fc3ba3e62df874cb8fd31a
Konrad <kondykox@gmail.com>
2024-05-21 13:49:44 +0200

    gamejolt link

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,13 +1,22 @@
 import Title from "../../components/Title/Title";
 import "./Header.css";
 
+const gamejoltLink = "https://gamejolt.com/@Kondy_Kox/games";
+
 const Header = ({ setGame }) => {
   return (
     <section className="header">
-      <img src="./logo.svg" alt="Logo" onClick={() => setGame(null)} />
-      <Title parentName={"header"} isMainTitle={true}>
-        Royal Karmel
-      </Title>
+      <div className="header__logo">
+        <img src="./logo.svg" alt="Logo" onClick={() => setGame(null)} />
+        <Title parentName={"header"} isMainTitle={true}>
+          Royal Karmel
+        </Title>
+      </div>
+      <div className="gamejolt">
+        <a href={gamejoltLink} target="_blank">
+          <img src="./gamejolt.svg" alt="Game Jolt" />
+        </a>
+      </div>
     </section>
   );
 };
